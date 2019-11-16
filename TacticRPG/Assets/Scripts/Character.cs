f9016eae408e35f59367bc29b12fdeb2699a4962
Wiratamana <wiratamana@icloud.com>
2019-11-16 19:11:10 +0900

    Bisa bikin Character pake scriptableobject

---
+++ TacticRPG/Assets/Scripts/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TacticRPG
+{
+    [CreateAssetMenu(fileName = "Character", menuName = "Create/Character", order = 1)]
+    public class Character : ScriptableObject
+    {
+        public Sprite sprite;
+        public int level;
+        public int hp;
+        public int mp;
+
+        public int attack;
+        public int defense;
+        public int magicAttack;
+        public int magicDefense;
+        public int luck;
+    }
+}
